fc032b0053a03d7797c9c2feaa6b9161a3bbcf95
winstonsigi <grant5073@gmail.com>
2021-06-07 22:10:40 +0300

    FIX

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -1,6 +1,7 @@
 package ru.javawebinar.topjava.util;
 
 import java.lang.reflect.Field;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
@@ -11,6 +12,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+
+
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExcess;
 
@@ -26,29 +29,28 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410)
         );
 
-        List<UserMealWithExcess> mealsTo = filteredByCycles(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
-        mealsTo.forEach(System.out::println);
+//        List<UserMealWithExcess> mealsTo = filteredByCycles(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+//        mealsTo.forEach(System.out::println);
 
-        System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
+//        System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
 
         test(); // FIXME: try to execute
     }
 
     public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime,
                                                             LocalTime endTime, int caloriesPerDay) {
-        if (meals.isEmpty()) return null; // FIXME: 1. 4to bi 4to? 2. may produce NullPointerException
+        if (meals == null) return new ArrayList<>(); // FIXME: 1. 4to bi 4to? 2. may produce NullPointerException
 
-        Map<Integer, Integer> mapCalories = new HashMap<>();
+        Map<LocalDate, Integer> mapCalories = new HashMap<>();
         for (UserMeal meal : meals) {
-            mapCalories.merge(meal.getDateTime().getDayOfYear(), meal.getCalories(), Integer::sum);
+            mapCalories.merge(meal.getDateTime().toLocalDate(), meal.getCalories(), Integer::sum);
         }
 
         List<UserMealWithExcess> mealExcess = new ArrayList<>();
         for (UserMeal meal : meals) {
-            if (meal.getDateTime().getHour() >= startTime.getHour()         // FIXME: see ru.javawebinar.topjava.util.TimeUtil#isBetweenHalfOpen(LocalTime lt, LocalTime startTime, LocalTime endTime)
-                    && meal.getDateTime().getHour() <= endTime.getHour()) { //
+            if (TimeUtil.isBetweenHalfOpen(meal.getDateTime().toLocalTime(), startTime, endTime)) {   // FIXME: see ru.javawebinar.topjava.util.TimeUtil#isBetweenHalfOpen(LocalTime lt, LocalTime startTime, LocalTime endTime)  { //
                 mealExcess.add(new UserMealWithExcess(meal.getDateTime(), meal.getDescription(), meal.getCalories(),
-                        caloriesPerDay < mapCalories.get(meal.getDateTime().getDayOfYear())));
+                        caloriesPerDay < mapCalories.get(meal.getDateTime().toLocalDate())));
             }
         }
         return mealExcess;
@@ -56,16 +58,16 @@ public class UserMealsUtil {
 
     public static List<UserMealWithExcess> filteredByStreams(List<UserMeal> meals, LocalTime startTime,
                                                              LocalTime endTime, int caloriesPerDay) {
-        if (meals.isEmpty()) return null; // FIXME: 1. 4to bi 4to? 2. may produce NullPointerException
+        if (meals == null) return new ArrayList<>(); // FIXME: 1. 4to bi 4to? 2. may produce NullPointerException
 
-        Map<Integer, Integer> mapCalories = meals.stream()
-            .collect(Collectors.groupingBy(meal -> meal.getDateTime().getDayOfYear(),
-                Collectors.summingInt(UserMeal::getCalories)));
+        Map<LocalDate, Integer> mapCalories = meals.stream()
+                .collect(Collectors.groupingBy(meal -> meal.getDateTime().toLocalDate(),
+                        Collectors.summingInt(UserMeal::getCalories)));
 
         return meals.stream()
                 .filter(meal -> TimeUtil.isBetweenHalfOpen(meal.getDateTime().toLocalTime(), startTime, endTime))
                 .map(meal -> new UserMealWithExcess(meal.getDateTime(), meal.getDescription(), meal.getCalories(),
-                        mapCalories.get(meal.getDateTime().getDayOfYear()) > caloriesPerDay))
+                        mapCalories.get(meal.getDateTime().toLocalDate()) > caloriesPerDay))
                 .collect(Collectors.toList());
     }
 
@@ -80,22 +82,22 @@ public class UserMealsUtil {
 
     public static void filteredByCyclesTest() throws InterruptedException {
         System.out.println("\n============= filteredByCyclesTest ============\n");
-        
+
         filteredByCycles(
-            Collections.emptyList(),
-            LocalTime.of(7, 0),
-            LocalTime.of(12, 0), 2000
+                Collections.emptyList(),
+                LocalTime.of(7, 0),
+                LocalTime.of(12, 0), 2000
         )
-            .forEach(System.out::println);
+                .forEach(System.out::println);
 
         List<UserMeal> meals = Arrays.asList(
-            new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500),
-            new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000),
-            new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500),
-            new UserMeal(LocalDateTime.of(2021, Month.JANUARY, 30, 0, 0), "Еда на граничное значение", 100),
-            new UserMeal(LocalDateTime.of(2021, Month.JANUARY, 30, 10, 0), "Завтрак", 1000),
-            new UserMeal(LocalDateTime.of(2021, Month.JANUARY, 30, 13, 0), "Обед", 500),
-            new UserMeal(LocalDateTime.of(2021, Month.JANUARY, 30, 20, 0), "Ужин", 410)
+                new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500),
+                new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000),
+                new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500),
+                new UserMeal(LocalDateTime.of(2021, Month.JANUARY, 30, 0, 0), "Еда на граничное значение", 100),
+                new UserMeal(LocalDateTime.of(2021, Month.JANUARY, 30, 10, 0), "Завтрак", 1000),
+                new UserMeal(LocalDateTime.of(2021, Month.JANUARY, 30, 13, 0), "Обед", 500),
+                new UserMeal(LocalDateTime.of(2021, Month.JANUARY, 30, 20, 0), "Ужин", 410)
         );
 
         List<UserMealWithExcess> mealsTo = filteredByCycles(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
@@ -103,10 +105,10 @@ public class UserMealsUtil {
         mealsTo.forEach(System.out::println);
 
         final UserMealWithExcess expected = new UserMealWithExcess(
-            meals.get(0).getDateTime(),
-            meals.get(0).getDescription(),
-            meals.get(0).getCalories(),
-            false);
+                meals.get(0).getDateTime(),
+                meals.get(0).getDescription(),
+                meals.get(0).getCalories(),
+                false);
 
         assertEquals(mealsTo.get(0), expected);
     }
@@ -115,20 +117,20 @@ public class UserMealsUtil {
         System.out.println("\n============= filteredByStreamsTest ============\n");
 
         filteredByStreams(
-            Collections.emptyList(),
-            LocalTime.of(7, 0),
-            LocalTime.of(12, 0), 2000
+                Collections.emptyList(),
+                LocalTime.of(7, 0),
+                LocalTime.of(12, 0), 2000
         )
-            .forEach(System.out::println);
+                .forEach(System.out::println);
 
         List<UserMeal> meals = Arrays.asList(
-            new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500),
-            new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000),
-            new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500),
-            new UserMeal(LocalDateTime.of(2021, Month.JANUARY, 30, 0, 0), "Еда на граничное значение", 100),
-            new UserMeal(LocalDateTime.of(2021, Month.JANUARY, 30, 10, 0), "Завтрак", 1000),
-            new UserMeal(LocalDateTime.of(2021, Month.JANUARY, 30, 13, 0), "Обед", 500),
-            new UserMeal(LocalDateTime.of(2021, Month.JANUARY, 30, 20, 0), "Ужин", 410)
+                new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500),
+                new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000),
+                new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500),
+                new UserMeal(LocalDateTime.of(2021, Month.JANUARY, 30, 0, 0), "Еда на граничное значение", 100),
+                new UserMeal(LocalDateTime.of(2021, Month.JANUARY, 30, 10, 0), "Завтрак", 1000),
+                new UserMeal(LocalDateTime.of(2021, Month.JANUARY, 30, 13, 0), "Обед", 500),
+                new UserMeal(LocalDateTime.of(2021, Month.JANUARY, 30, 20, 0), "Ужин", 410)
         );
 
         List<UserMealWithExcess> mealsTo = filteredByCycles(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
@@ -136,10 +138,10 @@ public class UserMealsUtil {
         mealsTo.forEach(System.out::println);
 
         final UserMealWithExcess expected = new UserMealWithExcess(
-            meals.get(0).getDateTime(),
-            meals.get(0).getDescription(),
-            meals.get(0).getCalories(),
-            false);
+                meals.get(0).getDateTime(),
+                meals.get(0).getDescription(),
+                meals.get(0).getCalories(),
+                false);
 
         assertEquals(mealsTo.get(0), expected);
     }
@@ -155,8 +157,8 @@ public class UserMealsUtil {
                 final Object expectedValue = field.get(expected);
                 if (!actualValue.equals(expectedValue)) {
                     throw new AssertionError(
-                        String.format("%nActual: %1$s=%2$s%nExpected: %1$s=%3$s",
-                            field.getName(), actualValue.toString(), expectedValue.toString())
+                            String.format("%nActual: %1$s=%2$s%nExpected: %1$s=%3$s",
+                                    field.getName(), actualValue.toString(), expectedValue.toString())
                     );
                 }
             }
