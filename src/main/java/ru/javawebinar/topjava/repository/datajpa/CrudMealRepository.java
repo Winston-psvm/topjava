bccd16b2da79e99685603b8943c2d56f9c6b94f3
winstonsigi <grant5073@gmail.com>
2021-08-23 17:00:31 +0300

    implement DataJpaMealRepository

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,7 +1,21 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+    @Transactional
+    @Modifying
+    int deleteByIdAndUserId(int id, int userId);
+
+    Meal findMealsByIdAndUserId(int id, int userId);
+
+    List<Meal> findAllByUserIdOrderByDateTimeDesc(int userId);
+
+    List<Meal> findAllByUserIdAndDateTimeGreaterThanEqualAndDateTimeLessThanOrderByDateTimeDesc(int user, LocalDateTime start, LocalDateTime end);
 }
