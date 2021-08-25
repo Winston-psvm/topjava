2456d16ef356375fc7894b2b04751ba74539e7cc
winstonsigi <grant5073@gmail.com>
2021-08-25 16:39:20 +0300

    add methods getWithUser and getWithMeals

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -2,6 +2,7 @@ package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 
@@ -18,4 +19,7 @@ public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
     List<Meal> findAllByUserIdOrderByDateTimeDesc(int userId);
 
     List<Meal> findAllByUserIdAndDateTimeGreaterThanEqualAndDateTimeLessThanOrderByDateTimeDesc(int user, LocalDateTime start, LocalDateTime end);
+
+    @Query("SELECT m from Meal m JOIN FETCH m.user WHERE m.id =?1 AND m.user.id=?2")
+    Meal getWithUser(int id, int userId);
 }
--- src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaMealServiceTest.java
@@ -1,10 +1,31 @@
 package ru.javawebinar.topjava.service.datajpa;
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.UserTestData;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.AbstractMealServiceTest;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
 
+import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.Profiles.DATAJPA;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
+import static ru.javawebinar.topjava.UserTestData.user;
 
 @ActiveProfiles(DATAJPA)
 public class DataJpaMealServiceTest extends AbstractMealServiceTest {
+    @Test
+    public void getWithUser() {
+        Meal meal = service.getWithUser(MEAL1_ID, USER_ID);
+        MATCHER.assertMatch(meal, meal1);
+        UserTestData.MATCHER.assertMatch(meal.getUser(),user);
+    }
+
+    @Test
+    public void getWithUserNotFound() {
+        Assert.assertThrows(NotFoundException.class,
+                () -> service.getWithUser(1, USER_ID));
+    }
 }
+
