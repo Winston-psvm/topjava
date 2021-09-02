0e0c4998a9d4876c1925a363205019450574cdc8
winstonsigi <grant5073@gmail.com>
2021-09-02 15:04:58 +0300

    fix jdbc tests

--- src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
@@ -3,15 +3,10 @@ package ru.javawebinar.topjava.service;
 import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.dao.DataAccessException;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.SqlConfig;
-import org.springframework.test.context.junit4.SpringRunner;
 import ru.javawebinar.topjava.UserTestData;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
@@ -35,6 +30,7 @@ public class AbstractUserServiceTest extends AbstractServiceTest{
     private CacheManager cacheManager;
 
     @Autowired
+    @Lazy
     protected JpaUtil jpaUtil;
 
     @Before
